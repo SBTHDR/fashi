9cfd85757f4e899cc2e2977cdf7a91dcf4494c9c
SBTHDR <subrata.halder13d@gmail.com>
2021-05-23 02:58:46 +0600

    admin login and logout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,67 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('backend/assets/img/apple-icon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('backend/assets/img/favicon.png') }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>
+        Admin Login
+    </title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <!-- CSS Files -->
+    <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('backend/assets/css/paper-dashboard.css?v=2.0.1') }}" rel="stylesheet" />
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="{{ asset('backend/assets/demo/demo.css') }}" rel="stylesheet" />
+</head>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 mx-auto">
+                <form method="POST" action="{{ route('login') }}" class="mt-5">
+                    @csrf
+                    <div class="card p-3" data-background="color" data-color="blue">
+                        <div class="card-header">
+                            <h3 class="card-title">Login</h3>
+                        </div>
+                        <div class="card-content">
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" id="email" placeholder="Enter email" class="form-control input-no-border @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" id="password" placeholder="Password" class="form-control input-no-border @error('password') is-invalid @enderror" name="password" autocomplete="current-password">
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                        <div class="card-footer text-center">
+                            <button type="submit" class="btn btn-fill btn-wd mb-3">Login</button>
+                            <div class="forgot">
+                                <a href="#pablo">Forgot your password?</a>
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </div>
-@endsection
+
+</body>
+
+</html>
---
+++ resources/views/backend/partials/sidebar.blade.php
@@ -0,0 +1,94 @@
+<div class="sidebar" data-color="white" data-active-color="danger">
+    <div class="logo">
+        <a href="https://www.creative-tim.com" class="simple-text logo-mini">
+            <div class="logo-image-small">
+                <img src="{{ asset('backend/assets/img/logo-small.png') }}">
+            </div>
+            <!-- <p>CT</p> -->
+        </a>
+        <a href="https://www.creative-tim.com" class="simple-text logo-normal">
+            Fashi Admin
+            <!-- <div class="logo-image-big">
+              <img src="../assets/img/logo-big.png">
+            </div> -->
+        </a>
+    </div>
+    <div class="sidebar-wrapper">
+        <ul class="nav">
+            <li class="active">
+                <a href="./dashboard.html">
+                    <i class="nc-icon nc-bank"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li>
+                <a href="./icons.html">
+                    <i class="nc-icon nc-atom"></i>
+                    <p>Banner Management</p>
+                </a>
+            </li>
+            <li>
+                <a href="./map.html">
+                    <i class="nc-icon nc-box"></i>
+                    <p>Category Management</p>
+                </a>
+            </li>
+            <li>
+                <a href="./notifications.html">
+                    <i class="nc-icon nc-bag-16"></i>
+                    <p>Products Management</p>
+                </a>
+            </li>
+            <li>
+                <a href="./user.html">
+                    <i class="nc-icon nc-cart-simple"></i>
+                    <p>Cart Management</p>
+                </a>
+            </li>
+            <li>
+                <a href="./typography.html">
+                    <i class="nc-icon nc-basket"></i>
+                    <p>Order Management</p>
+                </a>
+            </li>
+            <li>
+                <a href="./tables.html">
+                    <i class="nc-icon nc-single-copy-04"></i>
+                    <p>Post Management</p>
+                </a>
+            </li>
+            <li>
+                <a href="./tables.html">
+                    <i class="nc-icon nc-bookmark-2"></i>
+                    <p>Post Category</p>
+                </a>
+            </li>
+            <li>
+                <a href="./typography.html">
+                    <i class="nc-icon nc-tag-content"></i>
+                    <p>Post Tag</p>
+                </a>
+            </li>
+            <li>
+                <a href="./typography.html">
+                    <i class="nc-icon nc-chat-33"></i>
+                    <p>Reviews Management</p>
+                </a>
+            </li>
+            <li>
+                <a href="./typography.html">
+                    <i class="nc-icon nc-single-02"></i>
+                    <p>User Management</p>
+                </a>
+            </li>
+
+
+            <li class="active-pro">
+                <a href="./upgrade.html">
+                    <i class="nc-icon nc-settings"></i>
+                    <p>Settings</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
